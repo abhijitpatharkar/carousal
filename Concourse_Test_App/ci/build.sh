c3174e51197a4f648f2b7d8f8c34b51e560fc794
abhijitpatharkar <33035659+abhijitpatharkar@users.noreply.github.com>
2017-10-24 11:33:28 +0530

    Update build.sh

--- Concourse_Test_App/ci/build.sh
+++ Concourse_Test_App/ci/build.sh
@@ -7,4 +7,4 @@ cd Concourse_Test_App
   ./mvnw clean package
 cd ..
 
-cp source-code/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar  build-output/.
+cp source-code/Concourse_Test_App/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar  build-output/.
